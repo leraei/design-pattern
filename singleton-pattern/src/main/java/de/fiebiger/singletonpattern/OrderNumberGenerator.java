9ccb9cb23e288c58daa6aaa8fab31bd18ad173ee
tfiebige <timo.fiebiger@immonet.de>
2016-03-14 17:01:32 +0100

    Added demo project for singleton pattern

---
+++ singleton-pattern/src/main/java/de/fiebiger/singletonpattern/OrderNumberGenerator.java
@@ -0,0 +1,46 @@
+package de.fiebiger.singletonpattern;
+
+/**
+ * Singleton class for creating new orderNumbers
+ * Created by tfiebige on 14.03.16.
+ */
+public class OrderNumberGenerator {
+
+	private static OrderNumberGenerator instance = new OrderNumberGenerator();
+
+	private int orderNumber;
+
+
+	/**
+	 * The singleton pattern needs the constructor to be private
+	 */
+	private OrderNumberGenerator() {
+		/* For this demo i decided to initialize the ordernumber always with 0.
+		You will run into problems when rebooting if you don't persist this value. */
+		orderNumber = 0;
+	}
+
+
+	/**
+	 * Call this static method to get an instance of this class. It will always be the same
+	 *
+	 * @return the singleton instance of this class
+	 */
+	public static OrderNumberGenerator getInstance() {
+		if (instance == null) {
+			instance = new OrderNumberGenerator();
+		}
+		return instance;
+	}
+
+
+	/**
+	 * This method generates a new and unique orderNumber
+	 *
+	 * @return the generated orderNumber
+	 */
+	public int generateNewOrderNumber() {
+		return ++orderNumber;
+	}
+
+}
